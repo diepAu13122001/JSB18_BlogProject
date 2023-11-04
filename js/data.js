46a8e8801144fe128b773409efe16a7b1293bb8f
Horizon <71920146+UnionofSovietSocialistRepublics@users.noreply.github.com>
2023-11-04 13:25:45 +0000

    a

--- js/data.js
+++ js/data.js
@@ -1,8 +1,8 @@
 postList = [
-    { title: "", content: "", picture: "", created_by: "", created_at: "" },
-    { title: "", content: "", picture: "", created_by: "", created_at: "" },
-    { title: "", content: "", picture: "", created_by: "", created_at: "" },
-    { title: "", content: "", picture: "", created_by: "", created_at: "" },
-    { title: "", content: "", picture: "", created_by: "", created_at: "" },
+    { title: "Breaking bad", content: "sususuususuususus", picture: "https://nosomovo.xyz/wp-content/uploads/2019/10/hinh-anh-20-11-y-nghia-11.jpeg", created_by: "Walter hartwell white", created_at: "9-11-2009" },
+    { title: "Breaking news: the ottoman empire is defeated in the cursade of varna", content: "sususuususuususus", picture: "https://nosomovo.xyz/wp-content/uploads/2019/10/hinh-anh-20-11-y-nghia-11.jpeg", created_by: "Walter hartwell white", created_at: "9-11-2009" },
+    { title: "Breaking bad", content: "sususuususuususus", picture: "https://nosomovo.xyz/wp-content/uploads/2019/10/hinh-anh-20-11-y-nghia-11.jpeg", created_by: "Walter hartwell white", created_at: "9-11-2009" },
+    { title: "Breaking bad", content: "sususuususuususus", picture: "https://nosomovo.xyz/wp-content/uploads/2019/10/hinh-anh-20-11-y-nghia-11.jpeg", created_by: "Walter hartwell white", created_at: "9-11-2009"},
+    { title: "Breaking bad", content: "sususuususuususus", picture: "https://nosomovo.xyz/wp-content/uploads/2019/10/hinh-anh-20-11-y-nghia-11.jpeg", created_by: "Walter hartwell white", created_at: "9-11-2009" },
   ];
   
