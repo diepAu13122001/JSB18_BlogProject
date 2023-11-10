01861b0cb696b0c6dd46c17925fbdcddf9b83c52
Âu Ngọc Diệp <91660142+diepAu13122001@users.noreply.github.com>
2023-11-10 17:30:12 +0700

    post list

--- js/postdetail.js
+++ js/postdetail.js
@@ -5,25 +5,28 @@ function getPost(title) {
     // find the current post
     if (post.title == title) {
       // set var curPost = post and save in local
-      localStorage.setItem("curPost", post);
-      // move post detail page
-      window.location.href = "../html/postdetail.html";
-      return;
+      console.log(post)
+      localStorage.setItem("curPost", JSON.stringify(post));
     }
   });
+  // move post detail page
+  location.href = "../html/postdetail.html";
 }
-
-// pro-code
-// getPost('Minor annoyence');
 
 // add post in postdetail page
 function setPostInformation() {
+  // pro-code
+  // getPost("Minor annoyence");
   // get post detail = curPost
   let post = JSON.parse(localStorage.getItem("curPost"));
 
   // set author information
   document.getElementsByClassName("post-author")[0].innerHTML = post.created_by;
 
+  //set image link
+  document.querySelector(".post-pic img").src = post.picture;
+
+  
 }
 
 // execute function set post information
